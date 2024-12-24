65e230e46083646b96cc431fc2487290d1b05809
deepalid2808@gmail.com <703407032@genpact.com>
2024-12-24 17:46:14 +0530

    Created a method to return a record in UserController

--- src/main/java/git_test/controller/UserController.java
+++ src/main/java/git_test/controller/UserController.java
@@ -1,5 +1,21 @@
 package git_test.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+	
+	@GetMapping("/greet")
+	public String greet() {
+		return "Welcome to git";
+	}
+	
+	@GetMapping("/greet")
+	public String getOneRecord() {
+		return "record";
+	}
 
 }
